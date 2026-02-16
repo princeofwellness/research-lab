ba58b5aeda5ab08a76e35c0468e035592b834227
Prince of Wellness <tieplee@sorrywecan.com>
2026-02-16 09:53:50 -0600

    language switcher: preserve scroll position on locale change

--- components/LanguageChooser.vue
+++ components/LanguageChooser.vue
@@ -1,5 +1,13 @@
 <script setup lang="ts">
 const { locale, setLocale } = useI18n()
+
+const switchLang = async (lang: string) => {
+  const scrollY = window.scrollY
+  await setLocale(lang)
+  await nextTick()
+  window.scrollTo(0, scrollY)
+  requestAnimationFrame(() => window.scrollTo(0, scrollY))
+}
 </script>
 
 <template>
@@ -7,13 +15,13 @@ const { locale, setLocale } = useI18n()
     <a
       :class="[locale === 'en' ? 'font-bold' : 'font-medium opacity-50 hover:opacity-100']"
       href="javascript:void(0)"
-      @click="setLocale('en')"
+      @click="switchLang('en')"
     >EN</a>
     <span class="text-black/20">|</span>
     <a
       :class="[locale === 'sk' ? 'font-bold' : 'font-medium opacity-50 hover:opacity-100']"
       href="javascript:void(0)"
-      @click="setLocale('sk')"
+      @click="switchLang('sk')"
     >SK</a>
   </div>
 </template>
